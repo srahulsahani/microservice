151668d7764e810387ea219ba565d7ef7ec76784
Rahul Sahani <srahulsahani@gmail.com>
2024-01-29 23:05:06 +0530

    getStudents API | get all students

--- microservices-springboot/src/main/java/com/microservice/microservicesspringboot/controller/StudentController.java
+++ microservices-springboot/src/main/java/com/microservice/microservicesspringboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.microservice.microservicesspringboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -12,4 +15,15 @@ public class StudentController {
         Student student = new Student(1,"Ramesh","Fadatare");
         return student;
     };
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1,"Ramesh","Fadatare"));
+        students.add(new Student(1,"Umesh","Fadatare"));
+        students.add(new Student(1,"Ram","Jadhav"));
+        students.add(new Student(1,"Sanjay","Pawar"));
+
+        return students;
+    }
 }
