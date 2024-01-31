91d07a5d43a6fac4f995f1a5a7eb8c78aa76049b
Rahul Sahani <srahulsahani@gmail.com>
2024-01-31 23:34:11 +0530

    creating User entity

---
+++ microservices-springboot/src/main/java/com/microservice/microservicesspringboot/entity/User.java
@@ -0,0 +1,25 @@
+package com.microservice.microservicesspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false,unique = true)
+    private String email;
+}
